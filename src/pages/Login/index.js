0d238dabef86df67122063b2b3151d07657ccb08
zaryabdev <zaryabdev404@gmail.com>
2022-10-06 17:44:57 +0500

    added: animation, background-img, styles to login page

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,83 +1,135 @@
 import React from "react";
+import Lottie from "react-lottie";
 
-export default function Login(params) {
+import mainBackgourndSvg from "../../assets/images/login/Rectangle_3305.svg";
+import formBackgroundSvg from "../../assets/images/login/Rectangle_3303.svg";
+import rechaptcha from "../../assets/images/login/g847.svg";
+import animationData from "../../assets/animation/login/108277.json";
+
+export default function Login() {
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+    renderer: "svg",
+  };
   return (
     <React.Fragment>
-      <div className="container-fluid">
-        {/* <div>
-          <img src="https://d4.alternativeto.net/kVHYQt_DxZ9nWUJN4kAajau11vtsCy5k8EiawQNCnCU/rs:fit:1200:1200:0/g:ce:0:0/YWJzOi8vZGlzdC9zLzhjNzljYjExLWZkOTctZTMxMS04OWNlLTAwMjU5MGEwNWY1Zl8yX2Z1bGwuanBn.jpg" />
-          <img
-            class="top"
-            src="https://d4.alternativeto.net/kVHYQt_DxZ9nWUJN4kAajau11vtsCy5k8EiawQNCnCU/rs:fit:1200:1200:0/g:ce:0:0/YWJzOi8vZGlzdC9zLzhjNzljYjExLWZkOTctZTMxMS04OWNlLTAwMjU5MGEwNWY1Zl8yX2Z1bGwuanBn.jpg"
-          />
-        </div> */}
-        <div className="left-pane">
-          <div className="floating-img"></div>
-        </div>
-        <div className="right-pane">
-          <div className="floating-form">
-            <Form />
-          </div>
-        </div>
-      </div>
-    </React.Fragment>
-  );
-}
-
-function Form(params) {
-  return (
-    <React.Fragment>
-      <div className="background-img">
-        <center>
-          <p class="h4">Sign in to your account</p>
-        </center>
-        <div class="mb-3">
-          <label for="exampleFormControlInput1" class="form-label">
-            Email address
-          </label>
-          <input
-            type="email"
-            class="form-control"
-            id="exampleFormControlInput1"
-            placeholder="name@example.com"
-          />
-        </div>
-        <div class="mb-3">
-          <label for="exampleFormControlInput1" class="form-label">
-            Password
-          </label>
-          <input
-            type="email"
-            class="form-control"
-            id="exampleFormControlInput1"
-            placeholder="name@example.com"
-          />
-        </div>
-        <center>
-          <p>Forgot password</p>
-        </center>
-        <center>
-          <div class="border">
-            <div class="form-check">
-              <input
-                class="form-check-input"
-                type="checkbox"
-                value=""
-                id="flexCheckChecked"
-                checked
+      <div id="login" className="container-fluid">
+        <div
+          className="row"
+          style={{
+            backgroundImage: `url(${mainBackgourndSvg})`,
+            width: "100vw",
+            height: "100vh",
+          }}
+        >
+          <div className="col-sm-6">
+            <div
+              className="d-flex align-items-center justify-content-center"
+              style={{
+                height: "100vh",
+              }}
+            >
+              <Lottie
+                isClickToPauseDisabled
+                options={defaultOptions}
+                height={350}
+                width={350}
               />
-              <label class="form-check-label" for="flexCheckChecked">
-                I am human
-              </label>
             </div>
-            <div className="recaptcha-img"></div>
-            <a href="">Privacy</a>
-            <a href="">Terms</a>
           </div>
-          <button type="button" class="btn btn-primary">
-            Sign in
-          </button>
-        </center>
+          <div className="col-sm-6">
+            <div
+              className="d-flex align-items-center justify-content-center"
+              style={{
+                backgroundImage: `url(${formBackgroundSvg})`,
+                height: "100vh",
+              }}
+            >
+              <div className="glass-background">
+                <div>
+                  <center>
+                    <p className="h5 text-light mb-3">
+                      Sign in to your account
+                    </p>
+                  </center>
+                  <div className="mb-3">
+                    <p className="mb-0 text-light">
+                      <small>Email address</small>
+                    </p>
+                    <input
+                      type="email"
+                      className="form-control rounded-3 "
+                      style={{
+                        background: "transparent",
+                      }}
+                    />
+                  </div>
+                  <div className="mb-3">
+                    <p className="mb-0 text-light">
+                      <small>Password</small>
+                    </p>
+                    <input
+                      type="password"
+                      className="form-control rounded-3 "
+                      style={{
+                        background: "transparent",
+                      }}
+                    />
+                  </div>
+                  <center>
+                    <p className="text-light">
+                      <small> Forgot password?</small>
+                    </p>
+                  </center>
+                  <div className="row px-2">
+                    <center>
+                      <div className="border rounded-3 ">
+                        <div className="d-flex justify-content-between align-items-center">
+                          <div className="m-2">
+                            <div className="form-check">
+                              <input
+                                className="form-check-input"
+                                type="checkbox"
+                                value=""
+                              />
+                              <p className="m-0  text-light">I am human</p>
+                            </div>
+                          </div>
+                          <div className="div">
+                            <div className="m-2">
+                              <div
+                                style={{
+                                  backgroundImage: `url(${rechaptcha})`,
+                                }}
+                              >
+                                <div
+                                  style={{
+                                    width: 56,
+                                    height: 37,
+                                  }}
+                                ></div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="row px-2">
+                        <button
+                          type="button"
+                          className="btn btn-primary rounded-4 mt-3"
+                        >
+                          Sign in
+                        </button>
+                      </div>
+                    </center>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </React.Fragment>
   );
